f9165affba6cfece02a3e63b64fecb8e03a3356b
Web Developer <webdeveloper.abdullah@gmail.com>
2024-09-15 13:39:24 +0200

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,8 @@
+var mainParent = document.getElementById("mainParent"),
+    fakeBtn    = document.getElementById("fakeBtn");
+
+window.onload = function () {
+    setTimeout(function () {
+        // fakeBtn.click();
+    }, 1000);
+}
